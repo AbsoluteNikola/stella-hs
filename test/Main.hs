c7c5d807cdafadc5c88265823e3b08b3ab006d13
Nikolay Rulev <n.rulev@tinkoff.ru>
2024-03-05 22:15:34 +0300

    bad tests check

--- test/Main.hs
+++ test/Main.hs
@@ -6,17 +6,20 @@ import System.Directory (listDirectory, getCurrentDirectory)
 import Data.Traversable (for)
 import Data.Text.IO
 import Prelude hiding (readFile)
-import Stella.Check (StellaError, runStellaChecker)
+import Stella.Check (StellaError, runStellaChecker, renderStellaError)
 import Stella.Check.Types (SType)
 import Stella.Ast.ParSyntax (myLexer, pProgram)
 import Data.Either (isRight)
 import Data.Maybe (catMaybes)
+import qualified Data.Text as T
+import Data.Either.Combinators (fromLeft')
+import Data.Text (isInfixOf)
 
 main :: IO ()
 main = do
   tests <- testsGen
   defaultMain tests
-  
+
 
 testsGen :: IO TestTree
 testsGen = do
@@ -42,7 +45,9 @@ okTestGen testName filePath = do
       pure Nothing
     Right checkerRes ->
       pure $ Just $ testCase testName $
-        isRight checkerRes @?= True
+        assertBool
+          ("Should not fail with: " <> (T.unpack . renderStellaError . fromLeft' $ checkerRes))
+          (isRight checkerRes)
 
 badTestsGen :: IO TestTree
 badTestsGen = do
@@ -52,16 +57,26 @@ badTestsGen = do
   badTests <- for testErrorsList $ \err -> do
     let errDir = badTestsDir <> err <> "/"
     testCasesList <- listDirectory errDir
-    tg <- for testCasesList $ \tc -> badTestGen tc err (errDir <> tc)
+    (catMaybes -> tg) <- for testCasesList $ \tc -> badTestGen tc err (errDir <> tc)
     pure $ testGroup err tg
   pure $ testGroup "Bad" badTests
 
 
-badTestGen :: FilePath -> String -> FilePath -> IO TestTree
-badTestGen testName _errorName filePath = do
-  content <- readFile filePath
-  pure $ testCase testName $
-    content @?= content
+badTestGen :: FilePath -> String -> FilePath -> IO (Maybe TestTree)
+badTestGen testName errorName filePath = do
+  res <- runChecker filePath
+  case res of
+    Left err -> do
+      print $ "Broken test, parser fail: " <> filePath <> "\nerror: " <> err
+      pure Nothing
+    Right checkerRes ->
+      pure $ Just $ testCase testName $
+        case checkerRes of
+          Right _ -> assertFailure "Test should not suncceed"
+          Left err -> do
+            let msg = "Checker should finish with " <> errorName <> " \nBut finished with \n" <>  T.unpack (renderStellaError err)
+            assertBool msg $
+              T.pack errorName `isInfixOf` renderStellaError err
 
 runChecker :: FilePath -> IO (Either String (Either StellaError SType))
 runChecker path = do
